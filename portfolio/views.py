68e686989fe3a7814a2bbd02b8dbb9dc02a91dfa
Ahmad Matar <amatar@unomaha.edu>
2018-02-03 10:39:35 -0600

    First change

--- portfolio/views.py
+++ portfolio/views.py
@@ -94,12 +94,55 @@ def stock_delete(request, pk):
    stocks = Stock.objects.filter(purchase_date__lte=timezone.now())
    return render(request, 'portfolio/stock_list.html', {'stocks': stocks})
 
+@login_required
+def investment_delete(request, pk):
+   investment = get_object_or_404(Stock, pk=pk)
+   investment.delete()
+   investments = Investment.objects.filter(purchase_date__lte=timezone.now())
+   return render(request, 'portfolio/investment_list.html', {'investments': investments})
+
+
+
+@login_required
+def investment_new(request):
+   if request.method == "POST":
+       form = InvestmentForm(request.POST)
+       if form.is_valid():
+           investment = form.save(commit=False)
+           investment.created_date = timezone.now()
+           investment.save()
+           investments = Investment.objects.filter(purchase_date__lte=timezone.now())
+           return render(request, 'portfolio/investment_list.html',
+                         {'investments': investments})
+   else:
+       form = StockForm()
+       # print("Else")
+   return render(request, 'portfolio/stock_new.html', {'form': form})
+
+
+
 
 @login_required
 def investment_list(request):
    investments = Investment.objects.filter(acquired_date__lte=timezone.now())
    return render(request, 'portfolio/investment_list.html', {'investments': investments})
 
+@login_required
+def investment_edit(request):
+    investment= get_object_or_404(Investment, pk=pk)
+    if request.method == "POST":
+        form = InvestmentForm(request.POST, instance=stock)
+        if form.is_valid():
+            investment = form.save()
+            # stock.customer = stock.id
+            investment.updated_date = timezone.now()
+            investment.save()
+            investment = Investment.objects.filter(purchase_date__lte=timezone.now())
+            return render(request, 'portfolio/investment_list.html', {'investments': investment})
+    else:
+        # print("else")
+        form = StockForm(instance=stock)
+    return render(request, 'portfolio/investment_edit.html', {'form': form})
 
 
 @login_required
